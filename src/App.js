1d56579626926555438b0646e335b9ea216cfcca
Victoria Ukatu <victoria.ukatu@gmail.com>
2020-05-23 10:04:34 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://www.testbytes.net/wp-content/uploads/2019/06/Untitled-8.png" alt="programming-meme" />
+      <p>
+        This is our awesome new app!
+      </p>
+    </div>
+  );
+}
+
+export default App;
